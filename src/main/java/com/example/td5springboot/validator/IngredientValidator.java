7bbcc899f023120840ce314d93089ebc7247b43e
nyamyjese <hei.jese.2@gmail.com>
2026-03-29 20:55:25 +0100

    feat:add IngredientValidator in the validator package

---
+++ src/main/java/com/example/td5springboot/validator/IngredientValidator.java
@@ -0,0 +1,24 @@
+package com.example.td5springboot.validator;
+
+import com.example.td5springboot.entity.Ingredient;
+import com.example.td5springboot.exception.BadRequestException;
+
+public class IngredientValidator {
+    public void validate (Ingredient ingredient){
+        if(ingredient.getName() == null || ingredient.getName().isBlank()){
+            throw new BadRequestException("NewIngredient.name cannot be null");
+        }
+
+        if(ingredient.getCategory() == null){
+            throw new BadRequestException("NewIngredient.category cannot be null");
+        }
+
+        if(ingredient.getPrice() == null){
+            throw new BadRequestException("NewIngredient.price cannot be null");
+        }
+
+        if(ingredient.getPrice() < 0){
+            throw new BadRequestException("NewIngredient.price cannot be negative");
+        }
+    }
+}
