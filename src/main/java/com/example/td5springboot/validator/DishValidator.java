88348f3912af009e73376c5f649ca42734ff78d8
nyamyjese <hei.jese.2@gmail.com>
2026-03-29 21:08:47 +0100

    feat:add DishValidator in the validator package

---
+++ src/main/java/com/example/td5springboot/validator/DishValidator.java
@@ -0,0 +1,41 @@
+package com.example.td5springboot.validator;
+
+import com.example.td5springboot.entity.Dish;
+import com.example.td5springboot.entity.Ingredient;
+import com.example.td5springboot.exception.BadRequestException;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class DishValidator {
+    public void validate (Dish dish) {
+        if(dish.getName() == null || dish.getName().isBlank()){
+            throw new BadRequestException("NewDish.name cannot be null");
+        }
+
+        if(dish.getDishType() == null){
+            throw new BadRequestException("NewDish.DishType cannot be null");
+        }
+
+        if(dish.getPrice() == null){
+            throw new BadRequestException("NewDish.DishPrice cannot be null");
+        }
+
+        if(dish.getPrice() <= 0){
+            throw new BadRequestException("NewDish.DishPrice cannot be negative");
+        }
+    }
+
+    public void validateIngredientsList(List<Ingredient> ingredients) {
+        if(ingredients == null){
+            throw new BadRequestException("Request body is mandatory");
+        }
+
+        for(Ingredient ingredient : ingredients){
+            if(ingredient.getId() == null){
+                throw new BadRequestException("NewDishIngredient.id cannot be null");
+            }
+        }
+    }
+}
