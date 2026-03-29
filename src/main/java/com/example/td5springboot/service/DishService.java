aecf17c94351bd9e2da8d3ce7e3274f6711fd14d
nyamyjese <hei.jese.2@gmail.com>
2026-03-29 21:40:17 +0100

    fix(service):update DishService using DishValidator

--- src/main/java/com/example/td5springboot/service/DishService.java
+++ src/main/java/com/example/td5springboot/service/DishService.java
@@ -2,10 +2,10 @@ package com.example.td5springboot.service;
 
 import com.example.td5springboot.entity.Dish;
 import com.example.td5springboot.entity.Ingredient;
-import com.example.td5springboot.exception.BadRequestException;
 import com.example.td5springboot.exception.NotFoundException;
 import com.example.td5springboot.repository.DishRepository;
 import com.example.td5springboot.repository.IngredientRepository;
+import com.example.td5springboot.validator.DishValidator;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -16,35 +16,35 @@ import java.util.List;
 public class DishService {
     private final DishRepository dishRepository;
     private final IngredientRepository ingredientRepository;
+    private final DishValidator dishValidator;
 
-    public DishService(DishRepository dishRepository, IngredientRepository ingredientRepository) {
+    public DishService(DishRepository dishRepository, IngredientRepository ingredientRepository, DishValidator dishValidator) {
         this.dishRepository = dishRepository;
         this.ingredientRepository = ingredientRepository;
+        this.dishValidator = dishValidator;
     }
 
     public List<Dish> getAllDishes() {
         return dishRepository.findAll();
     }
 
-    public Dish getDishById(Integer id) {
-        return dishRepository.findById(id)
+    public void getDishById(Integer id) {
+        dishRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Dish.id=" + id + " is not found"));
     }
 
-    public Dish updateDishIngredients(Integer dishId , List<Ingredient> ingredientsFromRequest){
-        if(ingredientsFromRequest == null){
-            throw new BadRequestException("Request body is mandatory");
-        }
+    public Dish updateDishIngredients(Integer dishId, List<Ingredient> ingredientsFromRequest) {
+        dishValidator.validateIngredientsList(ingredientsFromRequest);
 
         getDishById(dishId);
 
-        List<Ingredient> validateIngredients = new ArrayList<>();
+        List<Ingredient> validIngredients = new ArrayList<>();
         for (Ingredient ingredient : ingredientsFromRequest) {
-            if(ingredient.getId() != null){
-                ingredientRepository.findById(ingredient.getId()).ifPresent(validateIngredients::add);
+            if (ingredient.getId() != null) {
+                ingredientRepository.findById(ingredient.getId()).ifPresent(validIngredients::add);
             }
         }
 
-        return dishRepository.updateIngredients(dishId, validateIngredients);
+        return dishRepository.updateIngredients(dishId, validIngredients);
     }
 }
